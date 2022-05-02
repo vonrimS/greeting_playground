6a6287b9260f45993a33ade9aa81dead33eafb32
vonrimS <mr.mytab@gmail.com>
2022-05-02 16:44:08 +0200

    add translator

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from greetings import greets
+from translate import Translator
+
+translator = Translator(to_lang='pl')
+
 for g in greets:
-    print(g.title()+';)')
+    print(translator.translate(g).title())
